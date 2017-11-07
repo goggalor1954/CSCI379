8b3c83fae7e0cc3792ea428896d42cc9f6cc0219
James Roesemann <goggalor1954@gmail.com>
2017-11-06 20:54:53 -0500

    need to to one test to see if the server can comunicate with more than one system at once.

--- project_1/modified/TCP_client.py
+++ project_1/modified/TCP_client.py
@@ -3,23 +3,41 @@
 #Project 1
 #Modified TCP client
 
+#the TCP_server must be running before before this program runs.
+
 import sys
-
 from socket import *
-serverName='127.0.0.1' #change this to the address of the server
 
+#the default serverName is the loopback address
+serverName='127.0.0.1'
+
+# if this program is run with arguments the value of the first argumetn is assigned to serverName
+# if no arguments are given the default serverName is the loopback address.
+# if given an invalid or unreachable ip address as an argument the program will not run.
 if len(sys.argv)>1:
  serverName = sys.argv[1]
 
-
+#sends TCP traffic on port 9999
 serverPort = 9999
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
+
+# the first line intered is considered the sentance. the second line entered is considered the command. bolth are sent to the server.
 sentence = raw_input('Input sentence: ')
 clientSocket.send(sentence)
+
+# The valid commands are 
+# Upper: changes all letters in the string to upper case.
+# Lower: changes all letters in the string to lower case.
+# Swap: Change all the upper case letters to lower case letters and change all the lower case letters to uppercase letters.
+# Rev: reverse the order of the words in the string.
+# Cap: capitlize the first letter of the string. change all other letters to lower case.
+# KILL: Kills the process running on the server. Returns a message confirming this.
 command = raw_input('Type command Upper, Lower, Swap, Rev or Cap. Type KILL to kill the server process: ')
 clientSocket.send(command)
+
+#if both sentance and command were transmited and considered valid, the server performs the command transformation and returns a string. We then print the string.
 modifiedSentence = clientSocket.recv(1024)
 print 'From Server:', modifiedSentence
 clientSocket.close()
-#this works, but if sentance and command get sent at the same time theres a chance that it won't be recived right. maybe sen it on diffrent ports? or diffrent identifiers.?
+
--- project_1/modified/TCP_server.py
+++ project_1/modified/TCP_server.py
@@ -3,17 +3,23 @@
 #Project 1
 #Modified TCP server
 
+#this program must be running before running TCP_client
+
 from socket import *
+#recives TCP traffic on port 9999
 serverPort = 9999
 serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind(('', serverPort))
+# the socket listens for 2 trasnsmissons.
 serverSocket.listen(2)
 print 'The server is ready to receive'
 while True:
  connectionSocket, addr = serverSocket.accept()
+#sentence represnets the sent sentance. modifier represents the command to change the value of sentence. 
  sentence = connectionSocket.recv(1024)
  modifier = connectionSocket.recv(1024)
 
+#depending on the value of modifier, perform the apropriate modification and return the modified string.
  if modifier == 'Upper':
   modifiedSentence = sentence.upper()
  if modifier == 'Lower':
@@ -21,13 +27,15 @@ while True:
  if modifier == 'Swap':
   modifiedSentence = sentence.swapcase()
  if modifier == 'Cap':
-  modifiedSentence = sentence.capitalize() ## also sets the other letters to lower case.
+  modifiedSentence = sentence.capitalize()
  if modifier == 'Rev':
   modifiedSentence=' '.join(sentence.split(' ')[::-1])
+#if a command wit hte value KILL is recived, transmit a message stating the the server has been killed and close the socket.
  if modifier == 'KILL':
   connectionSocket.send('Server killed.')
   connectionSocket.close()
   break
+# if the value of modifier is not a valid commant, transmit a string indicationg that the user has entered an invalid command.
  if modifier not in {'Upper', 'Lower', 'Swap', 'Cap', 'Rev', 'KILL'}:
   modifiedSentence = 'You typed an invalid command.'
  connectionSocket.send(modifiedSentence)
